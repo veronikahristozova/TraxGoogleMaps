f38b4ef2c2983195babc56a24e715aff9a7b7345
Veronika Hristozova <veronikahristozova@192.168.0.107>
2016-10-13 13:24:55 +0300

    minor changes

--- TraxGoogleMaps/ViewController.swift
+++ TraxGoogleMaps/ViewController.swift
@@ -11,14 +11,33 @@ import GooglePlaces
 import GoogleMaps
 
 class ViewController: UIViewController, UISearchBarDelegate {
+    var resultsViewController: GMSAutocompleteResultsViewController?
+    var searchController: UISearchController?
+    var resultView: UITextView?
+
 
     @IBOutlet weak var googleMapsContainer: UIView!
     var googleMapsView: GMSMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        resultsViewController = GMSAutocompleteResultsViewController()
+        //resultsViewController?.delegate = self
+        
+        searchController = UISearchController(searchResultsController: resultsViewController)
+        searchController?.searchResultsUpdater = resultsViewController
+        
+        // Put the search bar in the navigation bar.
+        searchController?.searchBar.sizeToFit()
+        self.navigationItem.titleView = searchController?.searchBar
+        
+        // When UISearchController presents the results view, present it in
+        // this view controller, not one further up the chain.
+        self.definesPresentationContext = true
+        
+        // Prevent the navigation bar from being hidden when searching.
+        searchController?.hidesNavigationBarDuringPresentation = false
     }
-    
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
@@ -31,12 +50,38 @@ class ViewController: UIViewController, UISearchBarDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func searchPlace(_ sender: UIBarButtonItem) {
-        let searchController = UISearchController(searchResultsController: nil)
-        searchController.searchBar.delegate = self
-        self.present(searchController, animated: true, completion: nil)
-        
-    }
+//    @IBAction func searchPlace(_ sender: UIBarButtonItem) {
+//        let searchController = UISearchController(searchResultsController: nil)
+//        searchController.searchBar.delegate = self
+//        self.present(searchController, animated: true, completion: nil)
+//        
+//    }
 
+}
+extension ViewController: GMSAutocompleteResultsViewControllerDelegate {
+    func resultsController(_ resultsController: GMSAutocompleteResultsViewController,
+                           didAutocompleteWith place: GMSPlace) {
+        searchController?.isActive = false
+        // Do something with the selected place.
+        print("Place name: ", place.name)
+        print("Place address: ", place.formattedAddress)
+        print("Place attributions: ", place.attributions)
+    }
+    
+    
+    func resultsController(_ resultsController: GMSAutocompleteResultsViewController!,
+                           didFailAutocompleteWithError error: Error!){
+        // TODO: handle the error.
+        //print("Error: ", error.description)
+    }
+    
+    // Turn the network activity indicator on and off again.
+    func didRequestAutocompletePredictionsForResultsController(resultsController: GMSAutocompleteResultsViewController!) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictionsForResultsController(resultsController: GMSAutocompleteResultsViewController!) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
 }
 
